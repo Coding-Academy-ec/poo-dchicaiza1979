94990e56527aa66669dfa6eba39849c99c027f5c
dchicaiza1979 <chicaiza_dennis@hotmail.com>
2024-08-08 12:52:56 -0500

    resolve exercise

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ class Rectangulo:
         self.altura = altura
 
     def area(self):
+        return self.base * self.altura
         pass # implementación de la función con la forula de área de un rectángulo
 
 
@@ -11,7 +12,9 @@ class Circulo:
     pi = 3.141592653589793
 
     def __init__(self, radio):
+        self.radio = radio
         pass # inicialización de la variable radio
 
     def area(self):
+        return Circulo.pi * (self.radio ** 2)
         pass # implementación de la función con la forula de área de un círculo
